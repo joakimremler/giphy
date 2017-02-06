962683bfca11541af143aaa65963d5c7398a9bb6
Joakim <joakim@idrottskoll.se>
2017-02-06 10:22:24 +0100

    added some packets

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,8 @@
 import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
+import uglify from 'rollup-plugin-uglify';
+
 
 export default {
   entry: 'src/scripts/index.js',
@@ -6,16 +10,15 @@ export default {
   format: 'iife',
   sourceMap: true,
 
-  "plugins":[
+  plugins:[
     babel({
-      exclude:'node_madules/**'
+      exclude:'node_modules/**'
     }),
     commonjs(),
     resolve({
       jsnext: true,
       browser: true
     }),
-    uglify()
-
+    (process.env.NODE_ENV === 'production' && uglify())
   ]
 };
