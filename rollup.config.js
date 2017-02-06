5ada08ec951a3f652bf81ab4bb9fe1325d44570e
Joakim <joakim@idrottskoll.se>
2017-02-06 10:00:11 +0100

    added some packets

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,21 @@
+import babel from 'rollup-plugin-babel';
+
 export default {
   entry: 'src/scripts/index.js',
   dest: 'public/assets/scripts/giphy.js',
   format: 'iife',
-  sourceMap: true
+  sourceMap: true,
+
+  "plugins":[
+    babel({
+      exclude:'node_madules/**'
+    }),
+    commonjs(),
+    resolve({
+      jsnext: true,
+      browser: true
+    }),
+    uglify()
+
+  ]
 };
